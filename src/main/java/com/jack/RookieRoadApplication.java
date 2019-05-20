b50eea0d5dbe923443f7450d118c07ff47f759af
jack <wj903829182@sina.cn>
2019-05-20 20:37:57 +0800

    Initial commit

---
+++ src/main/java/com/jack/RookieRoadApplication.java
@@ -0,0 +1,13 @@
+package com.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RookieRoadApplication.class, args);
+	}
+
+}
