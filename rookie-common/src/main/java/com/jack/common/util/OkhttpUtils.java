2f45a538d151a5a32d45a12da8f00798a1000ea1
wj903829182@sina.cn <wj903829182@sina.cn>
2019-12-04 20:23:28 +0800

    引入Okhttp包工具

---
+++ rookie-common/src/main/java/com/jack/common/util/OkhttpUtils.java
@@ -0,0 +1,12 @@
+package com.jack.common.util;
+
+/**
+ * Created By Jack on 2019/12/4
+ *
+ * @author Jack
+ * @date 2019/12/4 20:17
+ * @Description:
+ * 网络请求工具类
+ */
+public class OkhttpUtils {
+}
