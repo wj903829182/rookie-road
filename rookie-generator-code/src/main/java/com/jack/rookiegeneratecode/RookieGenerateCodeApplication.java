b3f1e3bd2eb1e3229ebc6a29843b2aa27c03bee1
jack <wj903829182@sina.cn>
2020-01-12 09:55:54 +0800

    CSV文件的操作

---
+++ rookie-generator-code/src/main/java/com/jack/rookiegeneratecode/RookieGenerateCodeApplication.java
@@ -0,0 +1,13 @@
+package com.jack.rookiegeneratecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieGenerateCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RookieGenerateCodeApplication.class, args);
+	}
+
+}
---
+++ rookie-thymeleaf/src/main/java/com/jack/rookie/thymeleaf/RookieThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.jack.rookie.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RookieThymeleafApplication.class, args);
+	}
+
+}
